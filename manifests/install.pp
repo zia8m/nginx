a6c6ba3860c4d9a37028b4cea69a487ba01a370b
root <root@dev2auto2pm.exampledapm.com>
2017-12-12 04:05:04 -0500

    update install, config and system

---
+++ manifests/install.pp
@@ -0,0 +1,12 @@
+class nginx::install(
+  String $ensure = $::nginx::ensure,
+  String $package_name = $::nginx::package_name,
+) {
+  package { 'install_nginx':
+    ensure => $ensure,
+    name   => $package_name,
+
+  }
+  
+  
+}
---
+++ manifests/service.pp
@@ -0,0 +1,18 @@
+class nginx::service(
+  String $ensure       = $::nginx::service_ensure,
+  String $service_name = $::nginx::service_name,
+  String $pattern      = $::nginx::service_pattern,
+  Boolean $enable      = $::nginx::service_enable,
+  Boolean $hasstatus   = $::nginx::service_hasstatus,
+  Boolean $hasrestart = $::nginx::service_hasrestart,
+) {
+  service { 'nginx-service':
+    ensure => $ensure,
+    name => $service_name,
+    enable     => $enable,
+    hasstatus  => $hasstatus,
+    hasrestart => $hasrestart,
+    pattern    => $pattern,
+
+  }
+}
